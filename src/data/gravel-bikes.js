4baeabe2d31262093a9804d1cbd6ca249dc46e9e
AtanasovCode <a_ljupce@yahoo.com>
2022-10-22 02:04:11 +0200

    Added Gravel and Road Bike Data

---
+++ src/data/gravel-bikes.js
@@ -0,0 +1,102 @@
+import revoltAdvanced1 from '../images/products/gravel-bikes/revolt-advanced-1.jpg';
+import revoltAdvancedPro0 from '../images/products/gravel-bikes/revolt-advanced-pro-0.jpg';
+import revolt2 from '../images/products/gravel-bikes/revolt-2.jpg';
+import checkpointSl5 from '../images/products/gravel-bikes/checkpoint-sl-5.jpg';
+import checkpointSl6eTap from '../images/products/gravel-bikes/checkpoint-sl-6-etap.jpg';
+import checkpointSlr7 from '../images/products/gravel-bikes/checkpoint-slr-7.jpg';
+import divergeSportCarbon from '../images/products/gravel-bikes/diverge-sport-carbon.webp';
+import divergeStrPro from '../images/products/gravel-bikes/diverge-str-pro.webp';
+import sWorksDivergeStr from '../images/products/gravel-bikes/s-works-diverge-str.webp';
+
+const gravelBikes = [
+    {
+        category: "Gravel Bikes",
+        categoryId: "gravelbikes",
+        brand: "Giant",
+        id: "revolt-advanced-1",
+        name: "Revolt Advanced 1", 
+        price: 4000,
+        image: revoltAdvanced1,
+        description: "Fast gravel, rough gravel, pavement or dirt. However you choose to roll, this bike does it all. A new composite frameset and adjustable wheelbase make it more capable than ever."
+    },
+    {
+        category: "Gravel Bikes",
+        categoryId: "gravelbikes",
+        brand: "Giant",
+        id: "revolt-advanced-pro-0",
+        name: "Revolt Advanced Pro 0", 
+        price: 6200,
+        image: revoltAdvancedPro0,
+        description: "Gravel goals vary, but whether you want flat-out speed or smooth control, this pro-level flyer can do it all. Lightweight, compliant and ultra-efficient on roads, gravel and dirt, it’s built for endurance and confident handling so you can crush daily rides and big weekend adventures."
+    },
+    {
+        category: "Gravel Bikes",
+        categoryId: "gravelbikes",
+        brand: "Giant",
+        id: "revolt-2",
+        name: "Revolt 2", 
+        price: 1450,
+        image: revolt2,
+        description: "Ride roads, gravel or dirt with this all-new aluminum all-rounder. A composite fork gives it a smooth ride, and a flip chip lets you adjust wheelbase and tire clearance."
+    },
+    {
+        category: "Gravel Bikes",
+        categoryId: "gravelbikes",
+        brand: "Trek",
+        id: "checkpoint-sl-5",
+        name: "Checkpoint SL 5",
+        price: 3129,
+        image: checkpointSl5,
+        description: "Checkpoint SL 5 is a carbon gravel bike made for epic all-road adventures. It's built with lightweight OCLV Carbon, terrain-smoothing IsoSpeed, nimble-yet-stable progressive geometry, internal storage and loads of mounts. "
+    },
+    {
+        category: "Gravel Bikes",
+        categoryId: "gravelbikes",
+        brand: "Trek",
+        id: "checkpoint-sl-6-etap",
+        name: "Checkpoint SL 6 eTap",
+        price: 4500,
+        image: checkpointSl6eTap,
+        description: "The Checkpoint SL 6 eTap is a carbon gravel bike designed to take you to places you'd never reach on a standard road bike. A lightweight OCLV Carbon frame with gravel-smoothing IsoSpeed, a smart and smooth SRAM Rival AXS wireless electronic drivetrain, nimble-yet-stable "
+    },
+    {
+        category: "Gravel Bikes",
+        categoryId: "gravelbikes",
+        brand: "Trek",
+        id: "checkpoint-slr-7",
+        name: "Checkpoint SLR 7",
+        price: 8500,
+        image: checkpointSlr7,
+        description: "Checkpoint SLR 7 is a carbon gravel bike that's built for crushing the world's greatest gravel races. The lightweight 700 Series OCLV Carbon frame has gravel-smoothing IsoSpeed and a nimble-yet-stable progressive geometry. Plus, a Shimano GRX drivetrain that shifts smoothly even in the gnarliest gravel."
+    },
+    {
+        category: "Gravel Bikes",
+        categoryId: "gravelbikes",
+        brand: "Specialized",
+        id: "diverge-sport-carbon",
+        name: "Diverge Sport Carbon",
+        price: 3500,
+        image: divergeSportCarbon,
+        description: "Whether your goal is to escape on gravel back roads, far from cars and crowds, or drop the hammer at the front of your favorite gravel race, no bike does it better than the new Diverge. It's quite simply the fastest, most capable - and just maybe the most fun - bike we've ever made."
+    },
+    {
+        category: "Gravel Bikes",
+        categoryId: "gravelbikes",
+        brand: "Specialized",
+        id: "diverge-str-pro",
+        name: "Diverge STR Pro",
+        price: 9500,
+        image: divergeStrPro,
+        description: "With Future Shock suspension front and rear, the new Diverge STR delivers compliance without compromise. By suspending the rider—instead of the bike."
+    },
+    {
+        category: "Gravel Bikes",
+        categoryId: "gravelbikes",
+        brand: "Specialized",
+        id: "s-works-diverge-str",
+        name: "S-Works Diverge STR",
+        price: 14000,
+        image: sWorksDivergeStr,
+        description: "With Future Shock suspension front and rear, the new Diverge STR delivers compliance without compromise. By suspending the rider—instead of the bike—Future Shock technology increases control and efficiency ."
+    },
+]
---
+++ src/data/road-bikes.js
@@ -0,0 +1,136 @@
+import propelAdvancedDisc1 from '../images/products/road-bikes/propel-advanced-disk-1.jpg';
+import propelAdvancedSL0 from '../images/products/road-bikes/propel-advanced-sl-0.jpg';
+import trcAdvancedPro1 from '../images/products/road-bikes/trc-advanced-pro-1.jpg';
+import madoneSlrGen7 from '../images/products/road-bikes/madone-slr-gen-7.webp';
+import madoneSl7eTap from '../images/products/road-bikes/madone-sl-7-eTap.webp';
+import boone6 from '../images/products/road-bikes/boone6.jpg';
+import roubaixExpert from '../images/products/road-bikes/roubaix-expert.webp';
+import roubaixSport from '../images/products/road-bikes/roubaix-sport.webp';
+import tarmacSl7Expert from '../images/products/road-bikes/tarmac-sl7-expert.webp';
+import domaneAl4Disc from '../images/products/road-bikes/domane-al-4-disc.jpg';
+import defyAdvancedPro1 from '../images/products/road-bikes/defy-advanced-pro-1.jpg';
+import contendAr1 from '../images/products/road-bikes/contend-ar-1.jpg';
+
+
+const roadBikes = [
+    {
+        category: "Road Bikes",
+        categoryId: "roadbikes",
+        brand: "Giant",
+        id: "propel-advanced-disc-1",
+        name: "Propel Advanced Disc 1",
+        price: 4980,
+        image: propelAdvancedDisc1,
+        description: "On the attack, in a sprint, or cornering at speed. Efficiency meets total control in this wind-slicing missile. Engineered with AeroSystem Shaping technology, an integrated WheelSystem, and disc brakes that are both powerful and aerodynamic, it gets up to speed and stays there with minimal resistance."
+    },
+    {
+        category: "Road Bikes",
+        categoryId: "roadbikes",
+        brand: "Giant",
+        id: "propel-advanced-sl-0",
+        name: "Propel Advanced SL 0",
+        price: 12000,
+        image: propelAdvancedSL0,
+        description: "Unleash your sprint with 200 meters to go. Close a gap, chase back, launch a solo attack. This new aero road machine is made for the moments that count. More aero, more adjustable, more ways to win."
+    },
+    {
+        category: "Road Bikes",
+        categoryId: "roadbikes",
+        brand: "Giant",
+        id: "trc-advanced-pro-1",
+        name: "TRC Advanced Pro 1",
+        price: 3850,
+        image: trcAdvancedPro1,
+        description: "From daily training rides to your biggest race of the year, this updated composite road racer features new aero tube shaping and added tire clearance. Lightweight, stiff and ultra-efficient, it’s pure all-rounder performance to help you aim for the podium."
+    },
+    {
+        category: "Road Bikes",
+        categoryId: "roadbikes",
+        brand: "Giant",
+        id: "contend-ar-1",
+        name: "Contend AR 1",
+        price: 1950,
+        image: contendAr1,
+        description: "Some roads are smooth, others are rough. This smooth-riding all-rounder handles it all, taking you from the main route to more adventurous options with confidence and control."
+    },
+    {
+        category: "Road Bikes",
+        categoryId: "roadbikes",
+        brand: "Giant",
+        id: "defy-advanced-pro-1",
+        name: "Defy Advanced Pro 1",
+        price: 6500,
+        image: defyAdvancedPro1,
+        description: "From gran fondos to solo rides, this lightweight composite road bike features endurance geometry and built-in compliance. Smooth and fast, all day long."
+    },
+    {
+        category: "Road Bikes",
+        categoryId: "roadbikes",
+        brand: "Trek",
+        id: "madone-slr-gen-7",
+        name: "Madone SLR Gen 7",
+        price: 9049,
+        image: madoneSlrGen7,
+        description: "the Madone SLR 6 is a road racing machine that's serious about speed from the second it hits the tarmac. An 800 Series OCLV Carbon frame with exclusive IsoFlow technology adds an aerodynamic advantage, cuts weight and smooths the road ahead. "
+    },
+    {
+        category: "Road Bikes",
+        categoryId: "roadbikes",
+        brand: "Trek",
+        id: "madone-sl-7-eTap",
+        name: "Madone SL 7 eTap",
+        price: 7030,
+        image: madoneSl7eTap,
+        description: "The Madone SL 7 Disc eTap combines advanced aerodynamic performance and smooth ride quality with the quick, reliable shifting of a wireless electronic drivetrain. It's light, fast and incredibly sleek, and it comes with aero carbon wheels, a road-smoothing IsoSpeed decoupler and a SRAM Force power meter."
+    },
+    {
+        category: "Road Bikes",
+        categoryId: "roadbikes",
+        brand: "Trek",
+        id: "boone6",
+        name: "Boone 6",
+        price: 3849,
+        image: boone6,
+        description: "The Boone 6 Disc is a carbon cyclocross racing bike that's built for crushing any CX course – even the most legendary on the UCI circuit. The sleek, lightweight carbon frame has terrain-smoothing Rear IsoSpeed and is trimmed with parts that perform in any conditions."
+    },
+    {
+        category: "Road Bikes",
+        categoryId: "roadbikes",
+        brand: "Trek",
+        id: "domane-al4-disc",
+        name: "Domane AL4 Disc",
+        price: 1600,
+        image: domaneAl4Disc,
+        description: "Domane AL 4 Disc combines the smooth, comfortable qualities of an advanced alloy frame with the performance of higher-quality parts. It features an upgraded drive train and crisp hydraulic disc brakes for a fast road bike perfect for competitive group rides and even races."
+    },
+    {
+        category: "Road Bikes",
+        categoryId: "roadbikes",
+        brand: "Specialized",
+        id: "roubaix-expert",
+        name: "Roubaix Expert",
+        price: 8000,
+        image: roubaixExpert,
+        description: "The all-new Roubaix Expert now delivers compliance without compromise by introducing a radical new Future Shock 2.0 that gives you more control and damping options, a new Pavé seatpost that creates our most balanced Roubaix to date, aerodynamics that equal the Tarmac, and a Rider-First Engineered™ frame that’s shed some serious weight."
+    },
+    {
+        category: "Road Bikes",
+        categoryId: "roadbikes",
+        brand: "Specialized",
+        id: "roubaix-sport",
+        name: "Roubaix Sport",
+        price: 3500,
+        image: roubaixSport,
+        description: "Aerodynamics that equal the Tarmac, and a Rider-First Engineered™ frame that’s shed some serious weight. Is it still comfortable? Sure, but don’t call it a comfort bike, because performance was behind every engineering decision we made. This is the new Roubaix."
+    },
+    {
+        category: "Road Bikes",
+        categoryId: "roadbikes",
+        brand: "Specialized",
+        id: "tarmac-sl7-expert",
+        name: "Tarmac SL7 Expert",
+        price: 8300,
+        image: tarmacSl7Expert,
+        description: "With a light-weight, Rider-First Engineered™ frame, it delivers the best-possible ride characteristics, no matter the size. So no matter what you have planned for your new Tarmac, its ready to help you achieve your goals—PRs, KOMs, or podium top-steps. One bike to rule them all."
+    },
+]
